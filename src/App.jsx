dafb361589eb455577622f338177c5d7aaf759b2
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-06-13 20:30:53 -0300

    fix(navbar): Center align navbar items.
    - Updated `Header.jsx` to use Bootstrap utility classes for centering navbar items.
    - Applied Bootstrap flexbox utilities to ensure proper alignment and spacing.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
-    <>
-      <header>
-        <h1>Helow World!</h1>
-      </header>
-    </>
-  )
+    <nav className="navbar fixed-top bg-body-tertiary">
+      <div className="container-fluid">
+        <ul className="navbar-nav mx-auto d-flex flex-row fs-4 fw-semibold">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">O Studio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Serviços & Tratamentos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
