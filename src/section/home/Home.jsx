45de3b90f16858f28fba1345f0af6e99b216d135
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-06-15 16:54:01 -0300

    feat(home-section): Adjust hero section height for desktop and mobile views.
    - Ensured proper styling of the carousel images within the hero section.

--- src/section/home/Home.jsx
+++ src/section/home/Home.jsx
@@ -1,13 +1,15 @@
+// HomeSection.jsx
 import React from "react";
 import Slider from "react-slick";
-import "./styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const HomeSection = () => {
   const settings = {
     dots: true,
-    speed: 2500,
+    speed: 6000,
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
@@ -18,20 +20,37 @@ const HomeSection = () => {
   return (
     <div className="home-section">
       <Slider {...settings}>
-        <div className="carousel-image">
-          <img src="/src/assets/bride-hero-backgroundpng.png" alt="" />
+        <div className="carousel-image position-relative">
+          <img
+            src="/src/assets/bride-hero-backgroundpng.png"
+            alt=""
+            className="img-fluid w-100 h-100 object-fit-cover"
+          />
+          <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
-        <div className="carousel-image">
-          <img src="/src/assets/salon-beauty-image.png" alt="" />
+        <div className="carousel-image position-relative">
+          <img
+            src="/src/assets/salon-beauty-image.png"
+            alt=""
+            className="img-fluid w-100 h-100 object-fit-cover"
+          />
+          <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
-        <div className="carousel-image">
+        <div className="carousel-image position-relative">
           <img
             src="/src/assets/woman-at-salon-beautty-with-blond-hair.png"
             alt=""
+            className="img-fluid w-100 h-100 object-fit-cover"
           />
+          <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
-        <div className="carousel-image">
-          <img src="/src/assets/woman-irealexing-in-a-spar.png" alt="" />
+        <div className="carousel-image position-relative">
+          <img
+            src="/src/assets/woman-irealexing-in-a-spar.png"
+            alt=""
+            className="img-fluid w-100 h-100 object-fit-cover"
+          />
+          <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
       </Slider>
     </div>
