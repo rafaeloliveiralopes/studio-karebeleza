77edc8b9fcc660e4f3547f57f9b307f07f875467
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-06-15 17:13:28 -0300

    fix(home-section): Correct image paths to ensure proper loading in production.
    - Import images directly into the component to ensure they are bundled correctly.
    - Adjust paths to be relative to the project structure.

--- src/section/home/Home.jsx
+++ src/section/home/Home.jsx
@@ -1,4 +1,3 @@
-// HomeSection.jsx
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -6,10 +5,15 @@ import "slick-carousel/slick/slick-theme.css";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import brideHeroBackground from "/src/assets/bride-hero-backgroundpng.png";
+import salonBeautyImage from "/src/assets/salon-beauty-image.png";
+import womanAtSalonBeauty from "/src/assets/woman-at-salon-beautty-with-blond-hair.png";
+import womanRelaxingInSpa from "/src/assets/woman-irealexing-in-a-spar.png";
+
 const HomeSection = () => {
   const settings = {
     dots: true,
-    speed: 6000,
+    speed: 2500,
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
@@ -21,35 +25,19 @@ const HomeSection = () => {
     <div className="home-section">
       <Slider {...settings}>
         <div className="carousel-image position-relative">
-          <img
-            src="/src/assets/bride-hero-backgroundpng.png"
-            alt=""
-            className="img-fluid w-100 h-100 object-fit-cover"
-          />
+          <img src={brideHeroBackground} alt="" className="img-fluid w-100 h-100 object-fit-cover" />
           <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
         <div className="carousel-image position-relative">
-          <img
-            src="/src/assets/salon-beauty-image.png"
-            alt=""
-            className="img-fluid w-100 h-100 object-fit-cover"
-          />
+          <img src={salonBeautyImage} alt="" className="img-fluid w-100 h-100 object-fit-cover" />
           <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
         <div className="carousel-image position-relative">
-          <img
-            src="/src/assets/woman-at-salon-beautty-with-blond-hair.png"
-            alt=""
-            className="img-fluid w-100 h-100 object-fit-cover"
-          />
+          <img src={womanAtSalonBeauty} alt="" className="img-fluid w-100 h-100 object-fit-cover" />
           <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
         <div className="carousel-image position-relative">
-          <img
-            src="/src/assets/woman-irealexing-in-a-spar.png"
-            alt=""
-            className="img-fluid w-100 h-100 object-fit-cover"
-          />
+          <img src={womanRelaxingInSpa} alt="" className="img-fluid w-100 h-100 object-fit-cover" />
           <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
         </div>
       </Slider>
