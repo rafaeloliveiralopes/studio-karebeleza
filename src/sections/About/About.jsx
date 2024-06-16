84cf788eb316d9260eae8da9e2e990815a62ab43
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-06-16 13:42:58 -0300

    Style(Lorem ipsum): Add period mark to Lorem ipsum paragraph.

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className="about-section">
       <div className="container">
         <h2>Transformando Beleza em Experiência</h2>
-        <p>Aenean placerat. In vulputate urna eu arcu. Aliquam erat volutpat. Suspendisse potenti. Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere. Nullam lectus justo,</p>
+        <p>Aenean placerat. In vulputate urna eu arcu. Aliquam erat volutpat. Suspendisse potenti. Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere. Nullam lectus justo.</p>
       </div>
     </section>
   );
