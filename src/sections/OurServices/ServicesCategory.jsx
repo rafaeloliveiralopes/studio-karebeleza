eccdd2643a9873bd62e5815a06e13a03c89ddaa2
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-06-17 15:46:50 -0300

    style(services): Add text-secondary-emphasis to services title.

--- src/sections/OurServices/ServicesCategory.jsx
+++ src/sections/OurServices/ServicesCategory.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const ServiceCategory = ({ icon, title, services }) => (
   <div className="col-md-4 text-center mt-4">
     <img src={icon} alt={`${title} icon`} width={50} className="mb-3" />
-    <h5>{title}</h5>
+    <h5 className="fw-bold text-secondary-emphasis">{title}</h5>
     <ul className="list-unstyled">
       {services.map((service, index) => (
         <li key={index}>{service}</li>
